b3b69c26394e99c36df2f1aa2fd0e2f25b5ae789
DaryaShonoeva <darya_shon@mail.ru>
2021-08-10 16:38:51 +0300

    Create button

--- app/src/main/java/com/geekbrains/mvvmandkotlin/MainActivity.kt
+++ app/src/main/java/com/geekbrains/mvvmandkotlin/MainActivity.kt
@@ -2,11 +2,24 @@ package com.geekbrains.mvvmandkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    class MainActivity : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+            setContentView(R.layout.activity_main)
+
+            val textView: TextView = findViewById(R.id.textView)
+
+            val button: Button = findViewById(R.id.Button)
+            button.setOnClickListener {
+                textView.text = "Hello!"
+            }
+        }
     }
 }
